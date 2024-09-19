126351b5b98ac2b823dc72744e3f826062120189
vfrtist <vfrtist@aol.com>
2024-09-19 15:31:32 -0400

    Assignment 9

---
+++ module-9/ryanv_Mod9_3.js
@@ -0,0 +1,34 @@
+const theHidden = document.querySelectorAll('.covered');
+
+function make(item) { return document.createElement(item.toString()); }
+
+class Reveal_Button {
+    constructor(hidden_element) {
+        this.hidden_element = hidden_element;
+        this.container = hidden_element.parentNode;
+        this.button = make('button');
+        this.setup();
+    }
+
+    setup() {
+        this.button.innerHTML = 'Reveal';
+        this.button.setAttribute('type', 'button');
+        this.button.classList.add('reveal_button');
+        this.container.append(this.button);
+        this.button.addEventListener('click', (e) => {
+            this.hidden_element.classList.remove('covered');
+        });
+
+        this.button.addEventListener('mouseenter', () => {
+            this.hidden_element.classList.add('active')
+        })
+        this.button.addEventListener('mouseleave', () => {
+            this.hidden_element.classList.remove('active')
+        })
+    }
+}
+
+
+for (hidden of theHidden) {
+    new Reveal_Button(hidden);
+}
