8050efab37b1311f4ac76165c89f5e67840b0dd0
vfrtist <vfrtist@aol.com>
2024-09-27 15:22:11 -0400

    more stylings

--- module-12/ryanv_Mod12_2.js
+++ module-12/ryanv_Mod12_2.js
@@ -2,6 +2,8 @@ function make(item) { return document.createElement(item.toString()); }
 
 const book_list = document.querySelector('#book_list');
 const titles = Array.from(book_list.querySelectorAll('li'));
+const up = document.querySelector('#up');
+const down = document.querySelector('#down');
 
 function line_up() {
     book_list.innerHTML = '';
@@ -13,10 +15,12 @@ function line_up() {
 
 function line_down() {
     book_list.innerHTML = '';
-    reverse = titles.toReversed();
+    const reverse = titles.toReversed();
     for (let title of reverse) {
         book_list.append(title);
     }
     book_list.reversed = true;
-
 }
+
+up.addEventListener('click', line_up)
+down.addEventListener('click', line_down)
